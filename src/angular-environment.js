1091a8a064730f927643a246efadeab9001f8397
adrian.huminiuc <huminiuc.adrian@gmail.com>
2015-12-07 10:06:21 +0200

    Return the first domain match, for more consistent behaviour

--- src/angular-environment.js
+++ src/angular-environment.js
@@ -88,7 +88,7 @@ angular.module('environment', []).
 			angular.forEach(this.data.domains, function(v, k) {
 				angular.forEach(v, function(v) {
 					if (location.match('/' + v + '$/')) {
-						self.environment = k;
+						return self.environment = k;
 					}
 				});
 			});
