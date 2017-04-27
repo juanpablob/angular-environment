54095e9e215ac7ca0c1f1ec0c42f86056b356188
Richard Russell <richardr39@gmail.com>
2017-04-27 23:46:38 +0100

    Add start of unit tests.

---
+++ karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = function (config) {
+  config.set({
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher'
+    ],
+    frameworks: ['jasmine'],
+    singleRun: false,
+    autoWatch: true,
+    colors: true,
+    reporters: ['dots'],
+    browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
+    files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'src/angular-environment.js',
+      'test/*.js'
+    ],
+    logLevel: config.LOG_ERROR
+  });
+};
